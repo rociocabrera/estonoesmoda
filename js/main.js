10a30a673e5a75a69a011ff23d223851f46c75a5
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-07-12 14:50:25 -0300

    add total price to cart

--- js/main.js
+++ js/main.js
@@ -31,12 +31,21 @@ const priceProduct = (productName) => {
   return 0;
 };
 
+const getProductNames = () => {
+  let productNames = "";
+  for (let i = 0; i < availableProducts.length; i++) {
+    const product = availableProducts[i];
+    productNames += product.name + ", ";
+  }
+  return productNames;
+};
+
 const orderProduct = () => {
   let validProduct = false;
   let product = "";
 
   while (!validProduct) {
-    product = prompt("Enter the product you want to buy: dress, shirt, pants");
+    product = prompt("Enter the product you want to buy: " + getProductNames());
     validProduct = validateProduct(product);
     if (!validProduct) {
       alert("The entered product is not valid");
@@ -60,20 +69,54 @@ const orderQuantity = () => {
   return quantity;
 };
 
-const buyProducts = () => {
+const calculateCartTotal = (productCart) => {
   let total = 0;
+  for (let i = 0; i < productCart.length; i++) {
+    const item = productCart[i];
+    total += item.subtotal;
+  }
+  return total;
+};
+
+const showCartItems = (productCart) => {
+  let items = "The items in your cart are: \n \n";
+
+  console.log(productCart);
+  for (let i = 0; i < productCart.length; i++) {
+    const item = productCart[i];
+
+    const words = item.product.split(" ");
+    const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    const capitalizedProduct = capitalizedWords.join(" ");
+
+    items += item.quantity + " " + capitalizedProduct + " - Subtotal: $" + item.subtotal;
+    if (i < productCart.length - 1) {
+      items += "\n";
+    }
+  }
+  return items;
+};
+
+const buyProducts = () => {
+  const productCart = [];
+
   let keepBuying = true;
 
   while (keepBuying) {
     const product = orderProduct();
     const quantity = orderQuantity();
-    total += priceProduct(product) * quantity;
+
+    const itemToAdd = { product, quantity, subtotal: priceProduct(product) * quantity };
+    productCart.push(itemToAdd);
     keepBuying = confirm("Do you want to continue shopping?");
   }
 
-  return total;
+  return productCart;
 };
 
-const totalPrice = buyProducts();
+const productCart = buyProducts();
+const total = calculateCartTotal(productCart);
+const totalString = "\n \n" + "The total of your purchase is: $" + total;
+const cartOutput = showCartItems(productCart) + totalString;
 
-alert("The total price is: $" + totalPrice);
+alert(cartOutput);
