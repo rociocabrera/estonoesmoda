f704751ed1ddd35e4418fe94b48db4c1b9227656
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-08-02 17:04:29 -0300

    assign destructuration and add objects to array

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 const availableProducts = [
-  { id: "dress", title: "Dress Grey Square ", price: 5000, img: "dressgreysquare.webp" },
-  { id: "shirt", title: "Shirt Butterfly", price: 2000, img: "shirt-butterfly.jpg" },
-  { id: "pants", title: "Pant Bordeaux", price: 3000, img: "pant-bordeaux.jpg" },
+  { id: "grey-dress", title: "Grey Square Dress ", price: 5000, img: "dressgreysquare.webp" },
+  { id: "scorpion-dress", title: "Scorpion Dress", price: 6000, img: "scorpion-dress.jpg" },
+  { id: "spring-dress", title: "Spring Dress", price: 7000, img: "spring-dress.jpg" },
+  { id: "butterfly-shirt", title: "Butterfly Shirt", price: 2000, img: "shirt-butterfly.jpg" },
+  { id: "black-shirt", title: "Black Shirt", price: 3000, img: "black-shirt.jpg" },
+  { id: "pink-shirt", title: "Bby Pink Shirt", price: 4000, img: "bby-pink-shirt.jpg" },
+  { id: "bordeaux-pant", title: "Bordeaux Pant", price: 3000, img: "pant-bordeaux.jpg" },
+  { id: "brown-pant", title: "Brown Square Pant", price: 4000, img: "brown-pant-square.jpg" },
+  { id: "blue-pant", title: "Blue Jean", price: 5000, img: "bluejeans.jpg" },
 ];
 let productCart = [];
+
+const cartItems = document.getElementById("cart-items");
+const finishPurchaseButton = document.getElementById("finish-purchase");
+const totalContainer = document.getElementById("total-container");
+const cartEmpty = document.getElementById("cart-empty");
+const cartTotal = document.getElementById("cart-total");
+const cartCount = document.getElementById("cart-count");
+const productQuantityInput = (product) => document.getElementById(`${product.id}-quantity`);
+const productsSection = document.getElementById("products");
 
 const validateQuantity = (quantity) => {
   return !isNaN(quantity) && quantity > 0;
 };
 
 const calculateCartTotal = () => {
-  return productCart.reduce((total, item) => total + item.subtotal, 0);
+  return productCart.reduce((total, { subtotal }) => total + subtotal, 0);
 };
 
 const calculateCartCount = () => {
-  return productCart.reduce((count, item) => count + item.quantity, 0);
+  return productCart.reduce((count, { quantity }) => count + quantity, 0);
 };
 
 const findProductById = (productId) => {
@@ -34,79 +49,95 @@ const addToCart = (product, quantity) => {
 };
 
 const removeFromCart = (productId) => {
+  const product = findProductById(productId);
+  productCart = productCart.filter((item) => item.product.id !== product.id);
+  saveCart();
+  renderCart();
+};
+
+const showRemoveConfirmation = (productId) => {
   Swal.fire({
+    icon: "warning",
     title: "Are you sure to revome this product?",
-    text: "You won't be able to revert this!",
+    customClass: {
+      title: "swal2-title",
+    },
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+    // text: "You won't be able to revert this!",
     color: "#000",
     background: "#fff",
-    position: "center",
-    icon: "warning",
+    position: "top-end",
+    customClass: "swal-wide",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     confirmButtonText: "Yes, delete it!",
   }).then((result) => {
     if (result.isConfirmed) {
-      const product = findProductById(productId);
-      productCart = productCart.filter((item) => item.product.id !== product.id);
-
-      saveCart();
-      renderCart();
-
-      Swal.fire("Deleted!", "Your product has been deleted.", "success");
+      removeFromCart(productId);
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your product has been deleted.",
+        icon: "success",
+        color: "#000",
+        background: "#fff",
+        position: "top-end",
+        customClass: "swal-wide",
+      })("Deleted!", "Your product has been deleted", "success");
     }
   });
 };
 
-const renderCart = () => {
-  const cartItems = document.getElementById("cart-items");
-  cartItems.innerHTML = "";
-
-  const finishPurchaseButton = document.getElementById("finish-purchase");
-  const totalContainer = document.getElementById("total-container");
-
+const toggleCartButtons = () => {
   if (productCart.length > 0) {
-    document.getElementById("cart-empty").style.display = "none";
+    cartEmpty.style.display = "none";
     finishPurchaseButton.style.display = "flex";
     totalContainer.style.display = "flex";
   } else {
-    document.getElementById("cart-empty").style.display = "flex";
+    cartEmpty.style.display = "flex";
     finishPurchaseButton.style.display = "none";
     totalContainer.style.display = "none";
   }
+};
 
-  productCart.forEach((item) => {
+const renderCart = () => {
+  const count = calculateCartCount(productCart);
+  cartCount.innerHTML = count;
+
+  toggleCartButtons();
+
+  cartItems.innerHTML = "";
+  productCart.forEach(({ product, quantity, subtotal }) => {
     const cartItem = document.createElement("div");
     cartItem.className = "cart-container";
 
+    const { id, img, title, price } = product;
+
     cartItem.innerHTML = `<div class="image-cart">
-          <img src="/assets/products/${item.product.img}" alt="" />
+          <img src="/assets/products/${img}" alt="" />
         </div>
         <div class="products-container">
           <div>
-            <h2>${item.product.title}</h2>
+            <h2>${title}</h2>
           </div>
           <div>
-            <h2>Quantity: ${item.quantity}</h2>
+            <h2>Quantity: ${quantity}</h2>
           </div>
           <div>
-            <h2>Price: $${item.product.price}</h2>
+            <h2>Price: $${price}</h2>
           </div>
           <div>
-            <h2>Subtotal: $${item.subtotal}</h2>
+            <h2>Subtotal: $${subtotal}</h2>
           </div>
-          <div><button onclick="removeFromCart('${item.product.id}')" class="btn btn-success">Remove</button></div>
+          <div><button onclick="showRemoveConfirmation('${id}')" class="btn btn-success">Remove</button></div>
         </div>`;
     cartItems.appendChild(cartItem);
   });
 
   const total = calculateCartTotal(productCart);
-  const cartTotal = document.getElementById("cart-total");
   cartTotal.innerHTML = total;
-
-  const count = calculateCartCount(productCart);
-  const cartCount = document.getElementById("cart-count");
-  cartCount.innerHTML = count;
 };
 
 const saveCart = () => {
@@ -117,36 +148,33 @@ const saveCart = () => {
 const loadCart = () => {
   const cartString = localStorage.getItem("cart") || "[]";
   productCart = JSON.parse(cartString);
+  toggleCartButtons();
+};
 
-  const finishPurchaseButton = document.getElementById("finish-purchase");
-  const totalContainer = document.getElementById("total-container");
-
-  if (productCart.length <= 0) {
-    finishPurchaseButton.style.display = "none";
-    totalContainer.style.display = "none";
-  } else if (productCart.length > 0) {
-    finishPurchaseButton.style.display = "flex";
-    totalContainer.style.display = "flex";
-  }
+const showQuantityValidationAlert = () => {
+  Swal.fire({
+    icon: "error",
+    title: "Please enter a valid quantity",
+    // text: "You must enter a number greater than 0",
+    customClass: "swal-wide-error",
+    color: "#000",
+    background: "#fff",
+    confirmButtonText: "Ok",
+    position: "top",
+    timer: 6000,
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+  });
 };
 
 const buyProduct = (productId) => {
   const product = findProductById(productId);
-
-  const quantityInput = document.getElementById(`${product.id}-quantity`);
+  const quantityInput = productQuantityInput(product);
   const quantity = parseInt(quantityInput.value);
 
   if (!validateQuantity(quantity)) {
-    Swal.fire({
-      title: "Please enter a valid quantity",
-      icon: "error",
-      Text: "You must enter a number greater than 0",
-      color: "#000",
-      background: "#fff",
-      confirmButtonText: "Ok",
-      position: "top",
-      timer: 6000,
-    });
+    showQuantityValidationAlert();
     return;
   }
 
@@ -158,24 +186,22 @@ const buyProduct = (productId) => {
 };
 
 const renderProducts = () => {
-  const productsSection = document.getElementById("products");
-
-  availableProducts.forEach((product) => {
+  availableProducts.forEach(({ id, img, title, price }) => {
     const card = document.createElement("div");
     card.className = "col-lg-4 col-md-6 col-sm-12";
 
     card.innerHTML = `<div class="card">
-        <img src="/assets/products/${product.img}" class="card-img-top" alt="shirt" />
+        <img src="/assets/products/${img}" class="card-img-top" alt="shirt" />
         <div class="card-body">
-          <h5 class="card-title">${product.title}</h5>
-          <h5 class="card-title">$${product.price}</h5>
+          <h5 class="card-title">${title}</h5>
+          <h5 class="card-title">$${price}</h5>
           <div class="input-group mb-3">
             <label class="input-group-text">
               Quantity
             </label>
-            <input id="${product.id}-quantity" type="number" min="0" class="form-control" placeholder="0" />
+            <input id="${id}-quantity" type="number" min="0" class="form-control" placeholder="0" />
           </div>
-          <button onclick="buyProduct('${product.id}')" class="btn btn-success">
+          <button onclick="buyProduct('${id}')" class="btn btn-success">
             Add to cart
           </button>
         </div>
@@ -184,11 +210,7 @@ const renderProducts = () => {
   });
 };
 
-const finishPurchase = () => {
-  productCart = [];
-  saveCart();
-  renderCart();
-
+const showPurchaseSuccess = () => {
   Toastify({
     text: "Your purchase has been completed successfully",
     newWindow: true,
@@ -201,11 +223,15 @@ const finishPurchase = () => {
   }).showToast();
 };
 
+const finishPurchase = () => {
+  productCart = [];
+  saveCart();
+  renderCart();
+  showPurchaseSuccess();
+};
+
 window.onload = () => {
-  // console.log("The page has been loaded");
   renderProducts();
   loadCart();
   renderCart();
 };
-
-
