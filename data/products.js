7bcefcfca321fa74ca2fc7a6084e29b2cd52f173
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-08-03 13:20:48 -0300

    refactor js files

---
+++ data/products.js
@@ -0,0 +1,11 @@
+const availableProducts = [
+  { id: "grey-dress", title: "Grey Square Dress ", price: 5000, img: "dressgreysquare.webp" },
+  { id: "scorpion-dress", title: "Scorpion Dress", price: 6000, img: "scorpion-dress.jpg" },
+  { id: "spring-dress", title: "Spring Dress", price: 7000, img: "spring-dress.jpg" },
+  { id: "butterfly-shirt", title: "Butterfly Shirt", price: 2000, img: "shirt-butterfly.jpg" },
+  { id: "black-shirt", title: "Black Shirt", price: 3000, img: "black-shirt.jpg" },
+  { id: "pink-shirt", title: "Bby Pink Shirt", price: 4000, img: "bby-pink-shirt.jpg" },
+  { id: "bordeaux-pant", title: "Bordeaux Pant", price: 3000, img: "pant-bordeaux.jpg" },
+  { id: "brown-pant", title: "Brown Square Pant", price: 4000, img: "brown-pant-square.jpg" },
+  { id: "blue-pant", title: "Blue Jean", price: 5000, img: "bluejeans.jpg" },
+];
